cedcb0fb2e24fc6e92ebcac9d14b8819fbea3559
miya0001 <miya@wpist.me>
2017-02-20 14:48:33 +0900

    first commit

---
+++ miya-analytics.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Miya Analytics
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     miya-analytics
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Miya_Analytics
+ */
